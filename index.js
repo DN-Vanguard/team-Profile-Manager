c0aeb2f9d499893366df4758b82ab701bfd37327
DN-Vanguard <Domiltannguyen@outlook.com>
2021-07-29 17:30:54 -0700

    command lines to link to html page has been created and completed.

--- index.js
+++ index.js
@@ -177,3 +177,119 @@ const askforInternInfo = () => {
             return askforNextTeamMember();
         });
 }
+// File containing the beginning of the HTML code block
+const renderHTMLStart = () => {
+    return fs.readFileSync("./src/start.html", "utf8", (err, data) => {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        return data;
+    });
+}
+// File containing the HTML code to define an employee card specified by the 'type' passed in
+const readCard = (type) => {
+    return fs.readFileSync(`./src/${type}Card.html`, "utf8", (err, data) => {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        return data;
+    });
+}
+// Replace the identifying string in the HTML with the data point from the object
+const replaceDataValues = (obj, string) => {
+    string = string.replace("[NAME]", obj.getName());
+    string = string.replace("[IDNUMBER]", obj.getId());
+    string = string.replace("[EMAIL]", obj.getEmail());
+    string = string.replace("[EMAIL]", obj.getEmail());
+    return string;
+}
+// Process each object stored in 'teamMembers' array into a single HTML string with card data
+const renderHTMLCards = () => {
+    cardsString = "";
+
+    for(let i = 0; i < teamMembers.length; i++) {
+
+        let appendCard = "";
+        const employeeType = teamMembers[i].constructor.name;
+
+        switch(employeeType) {
+
+            case "Manager":
+                appendCard = readCard(employeeType);
+                appendCard = appendCard.replace("[OFFICENUMBER]", teamMembers[i].getOfficeNumber());
+                break;
+
+            case "Engineer":
+                appendCard = readCard(employeeType);
+                appendCard = appendCard.replace("[GITHUB]", teamMembers[i].getGithub());
+                appendCard = appendCard.replace("[GITHUB]", teamMembers[i].getGithub());
+                break;
+
+            case "Intern":
+                appendCard = readCard(employeeType);
+                appendCard = appendCard.replace("[SCHOOLNAME]", teamMembers[i].getSchool());
+                break;
+        }
+
+        cardsString += replaceDataValues(teamMembers[i], appendCard);
+    }
+
+    return cardsString;
+}
+// Read the file containing the ending of the HTML code block
+const renderHTMLEnd = () => {
+    return fs.readFileSync("./src/end.html", "utf8", (err, data) => {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        return data;
+    });
+}
+// Generate one string of HTML to be sent back to 'writeToFile'
+const generateHTML = () => {
+    return renderHTMLStart() +
+        renderHTMLCards() +
+        renderHTMLEnd();
+}
+// Generate one string of CSS to be sent back to 'writeToFile'
+const generateCSS = () => {
+    return fs.readFileSync("./src/style.css", "utf8", (err, data) => {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        return data;
+    });
+}
+// Create a new file saved to /dist folder
+const writeToFile = (fileName, string) => {
+    fs.writeFile(`./dist/${fileName}`, string, (err) => err ? console.error(err) : console.log('Success!'));
+}
+// Initialize the question process
+const init = () => {
+    inquirer
+        .prompt(managerQuestions)
+        .then((managerAnswers) => {
+
+            // Add a manager object to team members array
+            teamMembers.push(new Manager(managerAnswers));
+
+            return askforNextTeamMember();
+        })
+        .then(() => {
+
+            const htmlString = generateHTML();
+            writeToFile("index.html", htmlString);
+
+            const cssString = generateCSS();
+            writeToFile("style.css", cssString);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+init();
